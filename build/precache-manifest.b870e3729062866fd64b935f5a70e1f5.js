f8e6df7fcc4bfb267651c9db0134872711dc181f
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2020-09-03 21:20:51 +0300

    Add files via upload

---
+++ build/precache-manifest.b870e3729062866fd64b935f5a70e1f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d95d5c2069954b7d53ceb11dfa70bd0",
+    "url": "/Mantas-eng/Calculator.git/index.html"
+  },
+  {
+    "revision": "726f554c6b39432fbc4a",
+    "url": "/Mantas-eng/Calculator.git/static/css/main.acf4bcdb.chunk.css"
+  },
+  {
+    "revision": "e3e49bd1d11c2ebcebf9",
+    "url": "/Mantas-eng/Calculator.git/static/js/2.b2175eda.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Mantas-eng/Calculator.git/static/js/2.b2175eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "726f554c6b39432fbc4a",
+    "url": "/Mantas-eng/Calculator.git/static/js/main.49764a89.chunk.js"
+  },
+  {
+    "revision": "9d1cdabfe5468bbda610",
+    "url": "/Mantas-eng/Calculator.git/static/js/runtime-main.86948e29.js"
+  }
+]);
