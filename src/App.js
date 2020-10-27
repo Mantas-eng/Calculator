632fa365d5b98a4c13622fc57d47fff339a02ecc
Mantas-eng <68243914+Mantas-eng@users.noreply.github.com>
2020-10-27 11:44:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,36 +5,29 @@ import ExpenseForm from "./components/ExpenseForm";
 import Alert from "./components/Alert";
 import uuid from "uuid/v4";
 
-//useEffect let's perform side effects
-// runs after every render
-// first paramater -  callback function (runs after render)
-// second paramater - array - for letting react know when to run useEffect.
-// react re-renders when state has changed or props
+
 
 const initialExpenses = localStorage.getItem("expenses")
   ? JSON.parse(localStorage.getItem("expenses"))
   : [];
 function App() {
-  // ************* state values *******************
-  // all expenses, add expense
+
   const [expenses, setExpenses] = useState(initialExpenses);
-  // single expense
+
   const [charge, setCharge] = useState("");
-  // single amount
+ 
   const [amount, setAmount] = useState("");
-  // alert
+  
   const [alert, setAlert] = useState({ show: false });
-  // edit
+ 
   const [edit, setEdit] = useState(false);
-  // edit item
+
   const [id, setId] = useState(0);
-  // ************* useEffect *******************
+
   useEffect(() => {
     console.log("we called useEffect");
     localStorage.setItem("expenses", JSON.stringify(expenses));
   }, [expenses]);
-  // ************* functionality *******************
-  // handle charge
 
   const handleCharge = e => {
     setCharge(e.target.value);
@@ -52,7 +45,7 @@ function App() {
     }, 3000);
   };
 
-  // handle submit
+
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -72,25 +65,25 @@ function App() {
       setCharge("");
       setAmount("");
     } else {
-      // handle alert called
+
       handleAlert({
         type: "danger",
         text: `charge can't be empty value and amount value has to be bigger than zero`
       });
     }
   };
-  // clear all items
+
   const clearItems = () => {
     setExpenses([]);
     handleAlert({ type: "danger", text: "all items deleted" });
   };
-  // handle delete
+
   const handleDelete = id => {
     let tempExpenses = expenses.filter(item => item.id !== id);
     setExpenses(tempExpenses);
     handleAlert({ type: "danger", text: "item deleted" });
   };
-  // handle edit
+
   const handleEdit = id => {
     let expense = expenses.find(item => item.id === id);
     let { charge, amount } = expense;
